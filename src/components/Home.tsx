b5d41ae0dd6b3a230db70fee23125fe2c61d6110
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-02-16 10:30:27 +0530

    update home data with vscode hover feature

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react"
+
 
 const Home = () => {
+
+    const [showPopup, setShowPopup] = useState<Number | null>(null)
 
     const phoneAPIurl: string = `https://api.whatsapp.com/send/?phone=919785691226&amp;text&amp;type=phone_number&amp;app_absent=0`
     const emailURL: string = "https://mail.google.com/mail/?view=cm&amp;fs=1&amp;tf=1&amp;to=shrivastavsumit15@gmail.com"
@@ -18,31 +22,31 @@ const Home = () => {
         {
             label: "my number",
             variableName: "telephoneNum",
-            value: "+91 97856-XXXXX",
+            value: `"+91 97856-XXXXX"`,
             link: phoneAPIurl,
         },
         {
             label: "my e-mail",
             variableName: "email",
-            value: "shrivastavsumit15@gmail.com",
+            value: `"shrivastavsumit15@gmail.com"`,
             link: emailURL,
         },
         {
             label: "you can also see it on my Github page",
             variableName: "githubLink",
-            value: "https://github.com/shrivastavsumit",
+            value: `"https://github.com/shrivastavsumit"`,
             link: githubURL,
         },
         {
             label: "you can also see my portfolio",
             variableName: "portfolio",
-            value: "https://pushpendra-portfolio.netlify.app/",
+            value: `"https://pushpendra-portfolio.netlify.app"`,
             link: portFolio,
         },
         {
             label: "you can check my Linkedin Page",
             variableName: "linkedinPage",
-            value: "https://www.linkedin.com/in/pushpendra-shrivastav/",
+            value: `"https://www.linkedin.com/in/pushpendra-shrivastav"`,
             link: linkedinPage,
         },
     ];
@@ -57,9 +61,14 @@ const Home = () => {
                 {contactInfo.map((item, index) => (
                     <div key={index} className="mb-4">
                         <div className="text-gray-400">// {item.label}</div>
-                        <div className="mb-2">
+                        <div className="mb-2 relative">
                             <span className="text-blue-700">const </span>
-                            <span className="text-emerald-400">{item.variableName}</span>
+                            <span className="text-emerald-400"
+                                onMouseEnter={() => setShowPopup(index)}
+                                onMouseLeave={() => setShowPopup(null)
+                                }>{item.variableName}</span>
+
+                            {showPopup === index && <span className="absolute text-white bottom-5 left-13 bg-black rounded p-1">(property) <span className="text-emerald-400">ContactInfoItem.label:</span> <span className="text-[#fea55f]">String</span></span>}
                             <span className="text-gray-400"> = </span>
                             <a className="text-red-700" href={item.link} target="_blank">{item.value}</a>
                             <span className="text-gray-400">;</span>
